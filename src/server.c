0f82d4fe0a43ca77a5badc848f0b3437551aa588
Khaoula Najmeddine <knajmeddine@enseirb-matmeca.fr>
2025-03-18 15:42:18 +0100

    Adding first version of server

---
+++ src/server.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <dlfcn.h>
+#include <getopt.h>
+#include "player.h"
+#include "move.h"
+#include "graph.h"
+
+struct player_t player
+{
+    enum player_color_t player_color;
+    char const* (*get_player_name)();
+    void (*initialize)(unsigned int, struct graph_t*);
+    struct move_t (*play)(const struct move_t previous_move);
+    void (*finalize)();
+    void *library;
+};
+
+void assert_dlsym(){
+  char* error;
+  if((error=dlerror())!=NULL){
+    fputs(error,stderr);
+    exit(1);
+  }
+}
+
+void load_player(struct player_t player, char *lib){
+    void *library=dlopen(lib,RTLD_LAZY);
+    if(!library){
+      fputs(dlerror(),stderr);
+      exit(1);
+    }
+
+    player->library=library;
+    player->get_player_name=dlsym(library, "get_player_name");
+    assert_dlsym();
+    player->initialize=dlsym(library, "initialize");
+    assert_dlsym();
+    player->play=dlsym(library, "play");
+    assert_dlsym();
+    player->finalize=dlsym(library, "finalize");
+    assert_dlsym();
+
+  
+}
+
