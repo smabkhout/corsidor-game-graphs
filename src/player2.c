3d45439b537b72fb1f19d784946e708b06fab66e
Dina Rachid <drachid@bordeaux-inp.fr>
2025-03-18 14:22:06 +0000

    player2

---
+++ src/player2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "player.h"
+
+char const* get_player_name(){
+  char const * a= "Lola";
+  return a;
+}
+
+
+
+void initialize(unsigned int player_id, struct graph_t* graph){
+  (void) player_id;
+  (void)graph;
+}
+
+
+struct move_t play(const struct move_t previous_move){
+  return previous_move;
+}
+
+
+void finalize(){
+  int a=1;
+  (void)a;
+}
