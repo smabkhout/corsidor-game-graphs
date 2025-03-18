0d0c300c208b489889f1fb14cbeec987173c6632
MABKHOUT Salah-Eddine <Salah-Eddine.Mabkhout@bordeaux-inp.fr>
2025-03-18 18:05:56 +0100

    fff

--- src/player1_functions.c
+++ src/player1_functions.c
@@ -1,6 +1,17 @@
 #include "player1_functions.h"
 
-struct move_t best_move(struct move_t previous, struct graph_t* graph)
+struct move_t inverted_move(struct move_t move)
+{
+    struct move_t inverted = {.c = move.c, .t = 2, .m = move.m, .e = move.e};
+    if (move.t)
+}
+
+struct move_t* possible_moves(struct move_t previous, struct graph_t* graph)
+{
+
+}
+
+struct move_t move_to_obj(struct move_t previous, struct graph_t* graph)
 {
     
 }
