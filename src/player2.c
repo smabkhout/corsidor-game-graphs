91d800f5d09a110d9bad7dc79b2157af3a0d35f1
MABKHOUT Salah-Eddine <Salah-Eddine.Mabkhout@bordeaux-inp.fr>
2025-03-18 15:26:28 +0100

    correction

--- src/player2.c
+++ src/player2.c
@@ -1,25 +1,28 @@
-#include <stdio.h>
 #include "player.h"
 
-char const* get_player_name(){
+char const* get_player_name()
+{
   char const * a= "Lola";
   return a;
 }
 
 
 
-void initialize(unsigned int player_id, struct graph_t* graph){
+void initialize(unsigned int player_id, struct graph_t* graph)
+{
   (void) player_id;
   (void)graph;
 }
 
 
-struct move_t play(const struct move_t previous_move){
+struct move_t play(const struct move_t previous_move)
+{
   return previous_move;
 }
 
 
-void finalize(){
+void finalize()
+{
   int a=1;
   (void)a;
 }
