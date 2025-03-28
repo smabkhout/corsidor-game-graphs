6c0149b488f6018522d131fd9d541ae22b8c3320
Khaoula Najmeddine <knajmeddine@enseirb-matmeca.fr>
2025-03-28 16:38:57 +0100

    Fixing the board structure

--- src/board.c
+++ src/board.c
@@ -10,20 +10,24 @@ struct board_t {
    // int walls[BOARD_SIZE][BOARD_SIZE];
     int wall_count;
     struct graph_t* graph;
+    int size;
 };
 
 
-void board_init(struct board_t *board) {
-    for (int i = 0; i < BOARD_SIZE; i++) {
-        for (int j = 0; j < BOARD_SIZE; j++) {
+void board_init() {
+    struct board_t *board=malloc(sizeof(struct board_t));
+    board->moves=malloc(sizeof(struct move_t));
+    board->size=BOARD_SIZE;
+    for (int i = 0; i < board->size; i++) {
+        for (int j = 0; j < board->size; j++) {
             board->cells[i][j] = NO_COLOR; 
         }
     }
 }
 
 void display_board(struct board_t *board) {
-    for (int i = 0; i < BOARD_SIZE; i++) {
-        for (int j = 0; j < BOARD_SIZE; j++) {
+    for (int i = 0; i < board->size; i++) {
+        for (int j = 0; j < board->size; j++) {
             char symbol;
             switch (board->cells[i][j]) {
                 case BLACK: symbol = 'B'; break;
