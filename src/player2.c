ef1ef9a25545fc4614fc5141c109daa6b7bc581e
AMRAR Rafiq <ramrar@enseirb-matmeca.fr>
2025-03-28 17:49:01 +0100

    version basique de player

--- src/player2.c
+++ src/player2.c
@@ -1,28 +1,82 @@
-#include "player_functions.h"
+
+#include <gsl/gsl_spmatrix.h>
+#include "graph.h"
+
+#include "player.h"
+#include "move_functions.h"
+#include "move.h"
+#include "board.h"
+#include "graph.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
+void initialize(unsigned int player_id, struct graph_t* graph){
+  //
+}
+
 
 char const* get_player_name()
 {
-  char const * a= "Lola";
-  return a;
+  return "palyer1";
 }
 
 
 
-void initialize(unsigned int player_id, struct graph_t* graph)
-{
-  (void) player_id;
-  (void)graph;
+
+struct move_t play(const struct move_t previous_move) {
+    struct move_t move;
+    
+    move.c = previous_move.c; 
+    move.t = MOVE; 
+
+    unsigned int new_pos = previous_move.m;  
+
+    while (1) {
+        new_pos = (new_pos + 1) % BOARD_SIZE;  
+
+        
+        move = create_move(previous_move.c, MOVE, new_pos, NULL);
+
+        if (is_valid_move(&move, board->graph)) {
+            printf("Player %d moves to vertex %u\n", move.c, move.m);
+            break;  
+        }
+    }
+
+    return move; 
 }
 
-
-struct move_t play(const struct move_t previous_move)
-{
-  return previous_move;
-}
 
 
 void finalize()
 {
-  int a=1;
-  (void)a;
+
+}
+
+
+int main() {
+    unsigned int n = 4;  // Number of vertices in the graph
+    struct graph_t* graph = malloc(sizeof(struct graph_t));
+    initialize(graph , n , CYCLIC) ; 
+
+    
+    // Initialisation de la position initiale (par exemple à la position 0)
+    struct move_t previous_move;
+    previous_move.c = 1;  // Exemple de couleur de joueur
+    previous_move.t = MOVE;
+    previous_move.m = 0;  // Position initiale du joueur
+
+    // Le graphe pour l'exemple
+    struct board_t* board = malloc(sizeof(struct board_t));
+    board->graph = graph ; 
+
+    // Appel de la fonction play avec un mouvement initial
+    struct move_t new_move = play(previous_move);
+
+    // Affichage du résultat
+    printf("New move: Player %d moves to vertex %u\n", new_move.c, new_move.m);
+
+    return 0;
 }
