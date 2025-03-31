a54911138cb1adac6f3e798e0d49b0a7f7f28d21
Khaoula Najmeddine <knajmeddine@enseirb-matmeca.fr>
2025-03-31 14:00:26 +0200

    Simple errors

--- src/graph_functions.h
+++ src/graph_functions.h
@@ -11,7 +11,7 @@ struct graph_t* createGraph(unsigned int n, enum graph_type_t type);
 void initialize_graph(struct graph_t *graph, unsigned int n , enum graph_type_t type ); 
 
 void print_graph(struct graph_t *graph);
-size_t find(void* const s[], size_t size, void* c, );
+size_t find(void* const s[], size_t size, void* c);
 void graph_generate_T(int m, struct graph_t *g, int (*in_hexagon)(int l, int c, int m));
 
 void free_graph(struct graph_t *graph) ; 
