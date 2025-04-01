257021b205f0c436dceb9680dd9b8b969a65f216
Dina Rachid <drachid@bordeaux-inp.fr>
2025-04-01 14:03:39 +0000

    board.h

--- src/board.h
+++ src/board.h
@@ -17,5 +17,7 @@ struct board_t* board_init();
 //void display_board(struct board_t *board);
 void add_move_to_board(struct board_t* board, struct move_t move);
 void board_free(struct board_t* board);
-
+// Fonction pour générer des murs dans le jeu.
+// Cette fonction remplit le tableau d'arêtes 'e' avec des positions aléatoires.
+void generate_wall(struct edge_t e[2], struct board_t * board);
 #endif // _CORS_BOARD_H_
