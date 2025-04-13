4b89d3fe2ce5f878fdb1b3b876a03b1643eff2c5
MABKHOUT Salah-Eddine <Salah-Eddine.Mabkhout@bordeaux-inp.fr>
2025-04-13 17:08:01 +0300

    khedra incha2allah

--- src/board.c
+++ src/board.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "board.h"
 #include "graph_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-struct board_t*         board_init(){
-    struct board_t *board=malloc(sizeof(struct board_t));
-    board->moves=malloc(sizeof(struct move_t));
-    board->size_moves=0;
-    board->wall_count=0;
-    return board;
+struct board_t *board_init() {
+  struct board_t *board = malloc(sizeof(struct board_t));
+  board->moves = malloc(sizeof(struct move_t));
+  board->size_moves = 0;
+  board->wall_count = 0;
+  return board;
 }
 
 /*void display_board(struct board_t *board) {
@@ -18,7 +18,7 @@ struct board_t*         board_init(){
             switch (board->cells[i][j]) {
                 case BLACK: symbol = 'B'; break;
                 case WHITE: symbol = 'W'; break;
-                default: symbol = '.'; break; 
+                default: symbol = '.'; break;
             }
             printf("%c ", symbol);
         }
@@ -26,15 +26,14 @@ struct board_t*         board_init(){
     }
 }*/
 
-
-void add_move_to_board(struct board_t* board, struct move_t move){
-    if (move.t == WALL ){
-        board->wall_count++;
-
-    }
-    board->moves=realloc(board->moves,(board->size_moves+1)*sizeof(struct move_t));
-    board->moves[board->size_moves]=move;
-    board->size_moves++;
+void add_move_to_board(struct board_t *board, struct move_t move) {
+  if (move.t == WALL) {
+    board->wall_count++;
+  }
+  board->moves =
+      realloc(board->moves, (board->size_moves + 1) * sizeof(struct move_t));
+  board->moves[board->size_moves] = move;
+  board->size_moves++;
 }
 
 /*void board_free(struct board_t* board){
@@ -43,63 +42,70 @@ void add_move_to_board(struct board_t* board, struct move_t move){
     free(board);
 }*/
 
-void board_free(struct board_t* board) {
-    if (board) {
-        free(board->moves);
-        if (board->graph)
-            graph_free(board->graph); 
-        free(board);
+void board_free(struct board_t *board) {
+  if (board) {
+    free(board->moves);
+    if (board->graph) {
+      puts("board->graph is freed");
+      graph_free(board->graph);
     }
-    board = NULL;
+    free(board);
+  }
+  board = NULL;
 }
 
-
-
-//on doit voir comment on peut generer wall sans que graph soit en parametre pour l utiliser dans la fct play
+// on doit voir comment on peut generer wall sans que graph soit en parametre
+// pour l utiliser dans la fct play
 void generate_wall(struct edge_t e[2], struct board_t *board) {
-    (void)board;
-    e[0].fr = axial_to_index(1, 0, 5) ;
-    e[0].to=  axial_to_index(0,1,5);
-    e[1].to = axial_to_index(0,1,5);
-    e[1].fr = axial_to_index(0,-1,5);
+  (void)board;
+  e[0].fr = axial_to_index(1, 0, 5);
+  e[0].to = axial_to_index(0, 1, 5);
+  e[1].to = axial_to_index(0, 1, 5);
+  e[1].fr = axial_to_index(0, -1, 5);
 }
 
+int is_invalid(struct move_t move, struct board_t *board) {
+  if (!board || !board->graph)
+    return 1;
 
-int is_invalid(struct move_t move, struct board_t* board) {
-    if (!board || !board->graph) return 1;
+  struct graph_t *graph = board->graph;
+  vertex_t my_pos = graph->start[move.c];
+  vertex_t opp_pos = graph->start[(move.c + 1) % NUM_PLAYERS];
 
-    struct graph_t* graph = board->graph;
-    vertex_t my_pos = graph->start[move.c];
-    vertex_t opp_pos = graph->start[(move.c + 1) % NUM_PLAYERS];
+  if (move.t == MOVE) {
+    if (move.m >= graph->num_vertices)
+      return 1;
+    if (move.m == opp_pos)
+      return 1;
+    if (gsl_spmatrix_uint_get(graph->t, my_pos, move.m) == 7)
+      return 1;
+    if (gsl_spmatrix_uint_get(graph->t, my_pos, move.m) == 0)
+      return 1;
 
-    if (move.t == MOVE) {
-        if (move.m >= graph->num_vertices) return 1;
-        if (move.m == opp_pos) return 1;
-        if (gsl_spmatrix_uint_get(graph->t, my_pos, move.m) == 7) return 1;
-        if (gsl_spmatrix_uint_get(graph->t, my_pos, move.m) == 0) return 1;
+    return 0;
+  }
 
-        return 0;
+  else if (move.t == WALL) {
+    for (int i = 0; i < 2; i++) {
+      if (move.e[i].fr >= graph->num_vertices ||
+          move.e[i].to >= graph->num_vertices)
+        return 1;
     }
-
-    else if (move.t == WALL) {
-        for (int i = 0; i < 2; i++) {
-            if (move.e[i].fr >= graph->num_vertices || move.e[i].to >= graph->num_vertices)
-                return 1;
-        }
-        for (int i = 0; i < 2; i++) {
-            if (gsl_spmatrix_uint_get(graph->t, move.e[i].fr, move.e[i].to) == 0)
-                return 1;
-        }
-        if (move.e[0].fr != move.e[1].fr) return 1;
-
-        enum dir_t d0 = gsl_spmatrix_uint_get(graph->t, move.e[0].fr, move.e[0].to);
-        enum dir_t d1 = gsl_spmatrix_uint_get(graph->t, move.e[1].fr, move.e[1].to);
-
-        if (abs((int)d0 - (int)d1) != 1 && abs((int)d0 - (int)d1) != 5)
-            return 1;
-
-        return 0; 
+    for (int i = 0; i < 2; i++) {
+      if (gsl_spmatrix_uint_get(graph->t, move.e[i].fr, move.e[i].to) == 0)
+        return 1;
     }
+    if (move.e[0].fr != move.e[1].fr)
+      return 1;
 
-    return 1; 
+    enum dir_t d0 = gsl_spmatrix_uint_get(graph->t, move.e[0].fr, move.e[0].to);
+    enum dir_t d1 = gsl_spmatrix_uint_get(graph->t, move.e[1].fr, move.e[1].to);
+
+    if (abs((int)d0 - (int)d1) != 1 && abs((int)d0 - (int)d1) != 5)
+      return 1;
+
+    return 0;
+  }
+
+  return 1;
 }
--- src/player1.c
+++ src/player1.c
@@ -1,42 +1,39 @@
+#include "board.h"
 #include "graph.h"
 #include "player.h"
-#include "board.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include<time.h>
-#include <string.h>
 #include <gsl/gsl_spmatrix.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
-//enum graph_type_t type;
-static struct board_t *board = NULL;  
+// enum graph_type_t type;
+static struct board_t *board = NULL;
 /*static unsigned int player_id;
 static vertex_t previous_position;
 static int has_played = 0;*/
 
-
-char const* get_player_name()
-{
+char const *get_player_name() {
   srand(time(NULL));
   char *names[] = {"adam", "rafiq"};
   return names[rand() % 2];
 }
 
-
-
-void initialize(unsigned int id, struct graph_t* graph) {
+void initialize(unsigned int id, struct graph_t *g) {
   board = board_init();
-  board->graph = malloc(sizeof(struct graph_t));
-    if (!board->graph) {
-        fprintf(stderr, "Erreur allocation du graph\n");
-        exit(EXIT_FAILURE);
-    }
+  board->graph = createGraph(4, TRIANGULAR);
+  if (!board->graph) {
+    fprintf(stderr, "Erreur allocation du graph\n");
+    exit(EXIT_FAILURE);
+  }
+  *g = *(board->graph);
+  // copy_graph(board->graph, graph);
 
-    copy_graph(board->graph, graph); 
-
-  printf("Player %d initialized on graph with %u vertices and %u edges , and with %u objectives\n", id , board->graph-> num_vertices , board->graph->num_edges , board->graph->num_objectives);
-
+  printf("Player %d initialized on graph with %u vertices and %u edges , and "
+         "with %u objectives\n",
+         id, board->graph->num_vertices, board->graph->num_edges,
+         board->graph->num_objectives);
 }
-
 
 /*struct move_t play(const struct move_t previous_move) {
     vertex_t my_pos = graph1->start[player_id];
@@ -53,7 +50,8 @@ void initialize(unsigned int id, struct graph_t* graph) {
         prev_dir = 3;
     }
 
-    struct move_t move = find_best_move(graph1, my_pos, opp_pos, prev_dir, player_id);
+    struct move_t move = find_best_move(graph1, my_pos, opp_pos, prev_dir,
+player_id);
 
     if (gsl_spmatrix_uint_get(graph1->t, my_pos, opp_pos) > 0) {
         for (vertex_t jump = 0; jump < graph1->num_vertices; jump++) {
@@ -65,50 +63,45 @@ void initialize(unsigned int id, struct graph_t* graph) {
         }
     }
     if (move.t == MOVE) {
-        previous_position = my_pos;         
-        graph1->start[player_id] = move.m;  
-        has_played = 1;                     
+        previous_position = my_pos;
+        graph1->start[player_id] = move.m;
+        has_played = 1;
     }
     return move;
 }*/
 struct move_t play(const struct move_t previous_move) {
-    struct move_t move;
+  struct move_t move;
 
-    move.t = NO_TYPE;
-    move.c = previous_move.c == NO_COLOR ? BLACK : (previous_move.c + 1) % 2;
-    move.m = 0; 
-    move.e[0].fr = move.e[0].to = 0;
-    move.e[1].fr = move.e[1].to = 0;
+  move.t = NO_TYPE;
+  move.c = previous_move.c == NO_COLOR ? BLACK : (previous_move.c + 1) % 2;
+  move.m = 0;
+  move.e[0].fr = move.e[0].to = 0;
+  move.e[1].fr = move.e[1].to = 0;
 
-    printf("👻 Player %d plays a NO_TYPE move (mock behavior)\n", move.c);
+  printf("👻 Player %d plays a NO_TYPE move (mock behavior)\n", move.c);
 
-    if (board != NULL) {
-        add_move_to_board(board, move);
-    } else {
-        printf("Board non initialisé dans play()\n");
-    }
+  if (board != NULL) {
+    add_move_to_board(board, move);
+  } else {
+    printf("Board non initialisé dans play()\n");
+  }
 
-    return move;
+  return move;
 }
 
-
 /*void finalize() {
-    if (graph1) {  
-        gsl_spmatrix_uint_free(graph1->t);  
-        free(graph1->objectives);  
-        free(graph1);  
-        graph1 = NULL;  
+    if (graph1) {
+        gsl_spmatrix_uint_free(graph1->t);
+        free(graph1->objectives);
+        free(graph1);
+        graph1 = NULL;
     }
 }
 */
 
 void finalize() {
-    if (board) {
-        board_free(board);
-        board = NULL;
-    }
+  if (board) {
+    board_free(board);
+    board = NULL;
+  }
 }
-
-
-
-
