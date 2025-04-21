6ce0944d79456015b92ad6c9fc2ad57fa2a5f47c
Dina Rachid <drachid@bordeaux-inp.fr>
2025-04-21 21:53:04 +0100

    modifications dans player3

--- src/player3.c
+++ src/player3.c
@@ -7,14 +7,13 @@
 #include <string.h>
 #include <gsl/gsl_spmatrix.h>
 
-//enum graph_type_t type;
+
 static struct board_t *board = NULL;  
-/*static unsigned int player_id;
-static vertex_t previous_position;
-static int has_played = 0;*/
+
 
 static int best_order[5];
 static int index_objective ;
+static enum player_color_t my_color;
 
 char const* get_player_name()
 {
@@ -33,66 +32,48 @@ void initialize(unsigned int id, struct graph_t* graph) {
         fprintf(stderr, "Erreur allocation du graph\n");
         exit(EXIT_FAILURE);
     }
+    my_color = id;
 
     // copy_graph(board->graph, graph); 
     TSP(board->graph,best_order);
     index_objective=0;
   printf("Player %d initialized on graph with %u vertices and %u edges , and with %u objectives\n", id , board->graph-> num_vertices , board->graph->num_edges , board->graph->num_objectives);
 
+
 }
 
 
-/*struct move_t play(const struct move_t previous_move) {
-    vertex_t my_pos = graph1->start[player_id];
-    vertex_t opp_pos = graph1->start[(player_id + 1) % 2];
-
-    if (previous_move.t == MOVE && previous_move.c != player_id) {
-        opp_pos = previous_move.m;
-    }
-
-    enum dir_t prev_dir = NO_EDGE;
-    if (has_played) {
-        prev_dir = get_direction(previous_position, my_pos, graph1);
-    } else {
-        prev_dir = 3;
-    }
-
-    struct move_t move = find_best_move(graph1, my_pos, opp_pos, prev_dir, player_id);
-
-    if (gsl_spmatrix_uint_get(graph1->t, my_pos, opp_pos) > 0) {
-        for (vertex_t jump = 0; jump < graph1->num_vertices; jump++) {
-            if (gsl_spmatrix_uint_get(graph1->t, opp_pos, jump) > 0 &&
-                jump != my_pos && jump != opp_pos) {
-                move = make_move_move(player_id, jump);
-                break;
-            }
-        }
-    }
-    if (move.t == MOVE) {
-        previous_position = my_pos;         
-        graph1->start[player_id] = move.m;  
-        has_played = 1;                     
-    }
-    return move;
-}*/
-
-
-
-// apartir d ici 
 
 struct move_t play(const struct move_t previous_move) {
     struct move_t move ;
     move.t = MOVE;
     printf(" current position %d\n",board->current_positions[0]);
-    move.c = previous_move.c == NO_COLOR ? WHITE : (previous_move.c + 1) % 2;
+    move.c = my_color;
  //   printf("%dcolorr222",move.c);
     vertex_t pos_player=board->current_positions[move.c];
-    printf("la couleur du joueur %d\n",board->current_positions[0]);
+    vertex_t pos_opponent = board->current_positions[(my_color + 1) % NUM_PLAYERS];
+    printf("la couleur du joueur %d\n",move.c);
     printf("la position du joueur %d\n ",pos_player);
+    if (gsl_spmatrix_uint_get(board->graph->t, pos_player, pos_opponent) > 0) {
+        enum dir_t dir_to_opponent = gsl_spmatrix_uint_get(board->graph->t, pos_player, pos_opponent);
+
+        for (vertex_t i = 0; i < board->graph->num_vertices; ++i) {
+            if (gsl_spmatrix_uint_get(board->graph->t, pos_opponent, i) == dir_to_opponent && i != pos_player && i != pos_opponent) {
+                printf("🤸 Player %d jumps over the opponent to vertex %d\n", my_color, i);
+                move.m = i;
+                move.e[0].fr = move.e[0].to = 0;
+                move.e[1].fr = move.e[1].to = 0;
+                board->current_positions[my_color] = i;
+                add_move_to_board(board, move);
+                return move;
+            }
+        }
+    }
+    vertex_t target = board->graph->objectives[best_order[index_objective]];
     int d[board->graph->num_vertices];
     int prev[board->graph->num_vertices];
     int next[board->graph->num_vertices];
-    dijistra ( board->graph, pos_player, board->graph->objectives[index_objective] , d, prev, next);
+    dijistra ( board->graph, pos_player, target , d, prev, next);
     printf("la position de l objective %d\n",board->graph->objectives[index_objective]);
     move.m = next[pos_player]; 
     printf(" le sommet destinataire %d\n", move.m);
@@ -101,7 +82,18 @@ struct move_t play(const struct move_t previous_move) {
     move.e[1].fr = move.e[1].to = 0;
 
     printf("👻 Player %d plays  moves \n", move.c);
-
+    board->current_positions[my_color] = move.m;
+    if (move.m == board->graph->objectives[index_objective]) {
+        printf("🎯 Objective %d reached!\n", index_objective);
+        index_objective++;
+        // Tous les objectifs atteints ? Retourner à la case de départ
+        if ((unsigned int)index_objective >= board->graph->num_objectives) {
+            printf("🏁 All objectives visited. Returning to base!\n");
+            board->graph->objectives[0] = board->graph->start[my_color];
+            board->graph->num_objectives = 1;
+            index_objective = 0;
+        }
+    }
     if (board != NULL) {
         add_move_to_board(board, move);
     } else {
@@ -112,15 +104,6 @@ struct move_t play(const struct move_t previous_move) {
 }
 
 
-/*void finalize() {
-    if (graph1) {  
-        gsl_spmatrix_uint_free(graph1->t);  
-        free(graph1->objectives);  
-        free(graph1);  
-        graph1 = NULL;  
-    }
-}
-*/
 
 void finalize() {
     if (board) {
