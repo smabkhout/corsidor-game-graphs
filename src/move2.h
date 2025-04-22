98c9ebf8306b24d3063ac03737f9e86e379fd41f
DAMMOU Adam <Adam.Dammou@bordeaux-inp.fr>
2025-04-22 12:25:27 -0400

    test pour move2

--- src/move2.h
+++ src/move2.h
@@ -18,5 +18,6 @@ int valid_move(struct graph_t *g, struct player_tt *p, vertex_t target, vertex_t
 int valid_wall(struct graph_t *g, struct player_tt *p, struct move_t move);
 void place_wall(struct graph_t *g, struct player_tt *p, struct move_t move);
 int availableMoves(struct move_t moves[], struct graph_t *graph, struct player_tt *p ,vertex_t opponent);
+int availableMovess(struct move_t moves[], struct graph_t *graph, struct player_tt *p ,vertex_t opponent);
 int path_to_objective_exists(struct graph_t *g, vertex_t start, const vertex_t *objectives, size_t nb_obj);
 struct move_t* make_move_moove(enum player_color_t color, vertex_t dest) ;
